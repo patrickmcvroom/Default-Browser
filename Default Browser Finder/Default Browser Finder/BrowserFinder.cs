0f3225638777377aa9de6a56f727a9f49b474b8f
Patrick McVroom <patrickmcvroom@gmail.com>
2021-04-28 17:19:20 +0100

    Updated GetSystemDefaultBrowser method
    Returned just the last file in the directory

--- Default Browser Finder/Default Browser Finder/BrowserFinder.cs
+++ Default Browser Finder/Default Browser Finder/BrowserFinder.cs
@@ -17,18 +17,26 @@ namespace DefaultBrowserFinder
                 //set the registry key we want to open
                 regKey = Registry.ClassesRoot.OpenSubKey("HTTP\\shell\\open\\command", false);
 
+                Console.WriteLine(regKey.GetValue(null).ToString().ToLower());
+
                 //get rid of the enclosing quotes
-                _browser = regKey.GetValue(null).ToString().ToLower().Replace("" + (char)34, "");
+                _browser = regKey.GetValue(null).ToString().ToLower().Replace("\"", "");
+
+                Console.WriteLine(_browser);
+                
 
                 //check to see if the value ends with .exe (this way we can remove any command line arguments)
-                if (!_browser.EndsWith("exe"))
+                if(!_browser.EndsWith("exe"))
+                {
                     //get rid of all command line arguments (anything after the .exe must go)
                     _browser = _browser.Substring(0, _browser.LastIndexOf(".exe") + 4);
 
+                    _browser = _browser.Substring(_browser.LastIndexOf("\\") + 1);
+                }
             }
-            catch (Exception ex)
+            catch (Exception exception)
             {
-                _browser = string.Format("ERROR: An exception of type: {0} occurred in method: {1} in the following module: {2}", ex.GetType(), ex.TargetSite, this.GetType());
+                _browser = string.Format("ERROR: An exception of type: {0} occurred in method: {1} in the following module: {2}", exception.GetType(), exception.TargetSite, this.GetType());
             }
             finally
             {
--- Default Browser Finder/Default Browser Finder/Program.cs
+++ Default Browser Finder/Default Browser Finder/Program.cs
@@ -6,8 +6,6 @@ namespace DefaultBrowserFinder
     {
         static void Main(string[] args)
         {
-            //Console.WriteLine("Press the SPACE BAR to find your default browser");
-
             BrowserFinder browser = new BrowserFinder();
             Console.WriteLine(browser.GetSystemDefaultBrowser());
         }
