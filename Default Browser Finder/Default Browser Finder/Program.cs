7fd7dece00f6f33e480802f8a87e7b2251f10d77
Patrick McVroom <patrickmcvroom@gmail.com>
2021-04-29 15:00:26 +0100

    Updated Program Class
    Opened a different subkey directory.
    
    Removed instance of previous Browser finder class.

--- Default Browser Finder/Default Browser Finder/Program.cs
+++ Default Browser Finder/Default Browser Finder/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Microsoft.Win32;
 
 namespace DefaultBrowserFinder
 {
@@ -6,8 +7,48 @@ namespace DefaultBrowserFinder
     {
         static void Main(string[] args)
         {
-            BrowserFinder browser = new BrowserFinder();
-            Console.WriteLine(browser.GetSystemDefaultBrowser());
+            const string userChoice = @"Software\Microsoft\Windows\Shell\Associations\UrlAssociations\http\UserChoice";
+            string progId;
+            string browser;
+
+            using RegistryKey userChoiceKey = Registry.CurrentUser.OpenSubKey(userChoice);
+            if (userChoiceKey == null)
+            {
+                browser = "Unknown";
+                //break;
+            }
+            object progIdValue = userChoiceKey.GetValue("Progid");
+            if (progIdValue == null)
+            {
+                browser = "Unknown";
+                //break;
+            }
+            progId = progIdValue.ToString();
+            switch (progId)
+            {
+                case "IE.HTTP":
+                    browser = "Internet Explorer";
+                    break;
+                case "FirefoxURL":
+                    browser = "Mozilla Firefox";
+                    break;
+                case "ChromeHTML":
+                    browser = "Google Chrome";
+                    break;
+                case "OperaStable":
+                    browser = "Opera";
+                    break;
+                case "SafariHTML":
+                    browser = "Safari";
+                    break;
+                case "AppXq0fevzme2pys62n3e0fbqa7peapykr8v":
+                    browser = "Microsoft Edge";
+                    break;
+                default:
+                    browser = "Unknown";
+                    break;
+            }
+            Console.WriteLine("Your default browser is: " + browser);
         }
     }
 }
